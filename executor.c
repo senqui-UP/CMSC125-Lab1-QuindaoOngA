1a65b36c25de1408348e4494bb75cefb61f9e68e
aXONG476 <andydominicong765@gmail.com>
2026-05-20 00:18:15 +0800

    [Feat] implement external command execution using fork and execvp

--- executor.c
+++ executor.c
@@ -1,7 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
 #include "shell.h"
 
 void execute_command(Command *cmd, char *original_cmd) {
+
+    if (cmd->command == NULL)
+        return;
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        perror("fork failed");
+        return;
+    }
+
+    if (pid == 0) {
+
+        execvp(cmd->command, cmd->args);
+
+        perror("execvp failed");
+        exit(127);
+    }
+
+    else {
+
+        int status;
+        waitpid(pid, &status, 0);
+    }
 }
 
 void cleanup_background_jobs() {
-}
+}s
