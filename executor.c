6714485f8a1dfba47d276372962e6c132e347594
aXONG476 <andydominicong765@gmail.com>
2026-05-20 00:21:00 +0800

    [Feat] implement background process handling and cleanup

--- executor.c
+++ executor.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/wait.h>
 
 #include "shell.h"
@@ -19,6 +20,49 @@ void execute_command(Command *cmd, char *original_cmd) {
 
     if (pid == 0) {
 
+        /*
+         * INPUT REDIRECTION
+         */
+        if (cmd->input_file != NULL) {
+
+            int fd = open(cmd->input_file, O_RDONLY);
+
+            if (fd < 0) {
+                perror("open input file");
+                exit(1);
+            }
+
+            dup2(fd, STDIN_FILENO);
+
+            close(fd);
+        }
+
+        /*
+         * OUTPUT REDIRECTION
+         */
+        if (cmd->output_file != NULL) {
+
+            int flags = O_WRONLY | O_CREAT;
+
+            if (cmd->append)
+                flags |= O_APPEND;
+            else
+                flags |= O_TRUNC;
+
+            int fd = open(cmd->output_file,
+                          flags,
+                          0644);
+
+            if (fd < 0) {
+                perror("open output file");
+                exit(1);
+            }
+
+            dup2(fd, STDOUT_FILENO);
+
+            close(fd);
+        }
+
         execvp(cmd->command, cmd->args);
 
         perror("execvp failed");
@@ -33,4 +77,4 @@ void execute_command(Command *cmd, char *original_cmd) {
 }
 
 void cleanup_background_jobs() {
-}s
+}
